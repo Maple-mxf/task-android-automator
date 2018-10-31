01ede7b3307f87d9173f5f3c9bdc00f8c1e39130
maxfeng <m17793873123@163.com>
2018-10-31 10:33:47 +0800

    部分bug解决

--- src/main/java/one/rewind/android/automator/AndroidDeviceManager.java
+++ src/main/java/one/rewind/android/automator/AndroidDeviceManager.java
@@ -18,226 +18,262 @@ import java.util.concurrent.*;
  */
 public class AndroidDeviceManager {
 
-	public volatile static TaskType taskType = null;
+    private static Dao<SubscribeAccount, String> subscribeDao;
+
+    public volatile static TaskType taskType = null;
 
 
-	public volatile static boolean running = false;
+    public volatile static boolean running = false;
 
-	/**
-	 * 从接口传递过来的微信号
-	 */
-	public static BlockingQueue<String> originalAccounts = new LinkedBlockingDeque<>();
+    /**
+     * 从接口传递过来的微信号
+     */
+    public static BlockingQueue<String> originalAccounts = new LinkedBlockingDeque<>();
 
-	private AndroidDeviceManager() {
-	}
+    private AndroidDeviceManager() {
+    }
 
-	Logger logger = LoggerFactory.getLogger(AndroidDeviceManager.class);
+    Logger logger = LoggerFactory.getLogger(AndroidDeviceManager.class);
 
-	/**
-	 * key : udid
-	 * value: state
-	 */
-	public static ConcurrentHashMap<String, AndroidDevice> devices = new ConcurrentHashMap<>();
+    /**
+     * key : udid
+     * value: state
+     */
+    public static ConcurrentHashMap<String, AndroidDevice> devices = new ConcurrentHashMap<>();
 
-	private static AndroidDeviceManager instance;
+    private static AndroidDeviceManager instance;
 
-	public static final int DEFAULT_APPIUM_PORT = 47454;
+    public static final int DEFAULT_APPIUM_PORT = 47454;
 
-	public static final int DEFAULT_LOCAL_PROXY_PORT = 48454;
+    public static final int DEFAULT_LOCAL_PROXY_PORT = 48454;
 
-	public static AndroidDeviceManager getInstance() {
-		synchronized (AndroidDeviceManager.class) {
-			if (instance == null) {
-				instance = new AndroidDeviceManager();
-			}
-			return instance;
-		}
-	}
+    public static AndroidDeviceManager getInstance() {
+        synchronized (AndroidDeviceManager.class) {
+            if (instance == null) {
+                instance = new AndroidDeviceManager();
+            }
+            return instance;
+        }
+    }
 
-	/**
-	 * 获得可用的设备
-	 *
-	 * @return
-	 */
-	public static List<AndroidDevice> obtainAvailableDevices() {
-		synchronized (AndroidDeviceManager.class) {
-			List<AndroidDevice> availableDevices = new ArrayList<>();
-			devices.forEach((k, v) -> {
-				if (v.state.equals(AndroidDevice.State.INIT)) {
-					availableDevices.add(v);
-				}
-			});
-			return availableDevices;
-		}
-	}
-
-	/**
-	 * 任务分配
-	 *
-	 * @throws InterruptedException
-	 */
-	public void allotTask() throws InterruptedException {
-		running = true;
-		if (taskType == null) {
-			taskType = TaskType.SUBSCRIBE;
-		}
-		allotTask(taskType);
-
-		WechatAdapter.executor.shutdown();
-		while (!WechatAdapter.executor.isTerminated()) {
-			WechatAdapter.executor.awaitTermination(800, TimeUnit.SECONDS);
-			System.out.println("progress:   done   %" + WechatAdapter.executor.isTerminated());
-		}
-
-		System.out.println("=======所有任务'''订阅任务'''执行完毕");
-
-		Thread.sleep(10000);
-
-		WechatAdapter.executor = Executors.newFixedThreadPool(10);
-
-		taskType = TaskType.CRAWLER;
-		allotTask(taskType);
-
-		WechatAdapter.executor.shutdown();
-		while (!WechatAdapter.executor.isTerminated()) {
-			WechatAdapter.executor.awaitTermination(800, TimeUnit.SECONDS);
-			System.out.println("progress:   done   %" + WechatAdapter.executor.isTerminated());
-
-		}
-
-		WechatAdapter.futures.forEach(v -> System.out.println("v:" + v));
-
-		running = false;
-	}
+    /**
+     * 获得可用的设备
+     *
+     * @return
+     */
+    public static List<AndroidDevice> obtainAvailableDevices() {
+        synchronized (AndroidDeviceManager.class) {
+            List<AndroidDevice> availableDevices = new ArrayList<>();
+            devices.forEach((k, v) -> {
+                if (v.state.equals(AndroidDevice.State.INIT)) {
+                    availableDevices.add(v);
+                }
+            });
+            return availableDevices;
+        }
+    }
 
 
-	/**
-	 * 获取到可用的设备   开启线程池进行任务分配
-	 * 任务分配有两种: 关注公众号和抓取特定公众号文章
-	 *
-	 * @param type
-	 */
-	public void allotTask(TaskType type) {
-		List<AndroidDevice> availableDevices = obtainAvailableDevices();
-		if (TaskType.SUBSCRIBE.equals(type)) {
-			allotSubscribeTask(availableDevices);
-		} else if (TaskType.CRAWLER.equals(type)) {
-			allotCrawlerTask(availableDevices, false);
-		}
+    /**
+     * 任务分配
+     *
+     * @throws InterruptedException
+     */
+    @Deprecated
+    public void allotTask() throws InterruptedException {
+        running = true;
+        if (taskType == null) {
+            taskType = TaskType.SUBSCRIBE;
+        }
+        allotTask(taskType);
 
-	}
+        WechatAdapter.executor.shutdown();
+        while (!WechatAdapter.executor.isTerminated()) {
+            WechatAdapter.executor.awaitTermination(800, TimeUnit.SECONDS);
+            System.out.println("progress:   done   %" + WechatAdapter.executor.isTerminated());
+        }
 
-	/**
-	 * Description:  分配订阅公众号的任务
-	 *
-	 * @param availableDevices
-	 */
-	private void allotSubscribeTask(List<AndroidDevice> availableDevices) {
-		try {
-			for (AndroidDevice device : availableDevices) {
+        System.out.println("=======所有任务'''订阅任务'''执行完毕");
 
-				Dao<SubscribeAccount, String> dao = DaoManager.getDao(SubscribeAccount.class);
+        Thread.sleep(10000);
 
-				//查询总共关注了多少公众号  今天关注了多少
-				long countSub = dao.queryBuilder().where().
-						eq("udid", device.udid).countOf();
-				Calendar calendar = Calendar.getInstance();
-				calendar.setTime(new Date());
-				calendar.set(Calendar.HOUR_OF_DAY, 0);
-				calendar.set(Calendar.MINUTE, 0);
-				calendar.set(Calendar.SECOND, 0);
-				Date var0 = calendar.getTime();
-				Date var1 = DateUtils.addDays(var0, 1);
-				long currentSub = dao.queryBuilder().where().
-						eq("udid", device.udid).and().
-						between("insert_time", var0, var1).countOf();
-				if (countSub >= 920 || currentSub >= 40) {
-					continue;
-				}
+        WechatAdapter.executor = Executors.newFixedThreadPool(10);
 
-				//初始化设备的任务队列
-				device.queue.clear();
-				int rest = (int) (40 - currentSub);
-				for (int i = 0; i < rest; i++) { // TODO 测试改动
-					if (originalAccounts.size() == 0) break;
-					device.queue.add(originalAccounts.take());
-				}
-				//开启任务执行
-				if (device.queue.size() == 0) return;
+        taskType = TaskType.CRAWLER;
+        allotTask(taskType);
 
-				device.setClickEffect(false);
+        WechatAdapter.executor.shutdown();
+        while (!WechatAdapter.executor.isTerminated()) {
+            WechatAdapter.executor.awaitTermination(800, TimeUnit.SECONDS);
+            System.out.println("progress:   done   %" + WechatAdapter.executor.isTerminated());
 
-				WechatAdapter adapter = new WechatAdapter(device);
+        }
 
-				WechatAdapter.taskType = TaskType.SUBSCRIBE;
+        WechatAdapter.futures.forEach(v -> System.out.println("v:" + v));
 
-				adapter.start(false);
-
-				logger.info("任务初始化完毕！！！");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        running = false;
+    }
 
 
-	/**
-	 * Description:  抓取文章任务
-	 *
-	 * @param availableDevices
-	 * @param recovery         恢复机制
-	 */
-	public void allotCrawlerTask(List<AndroidDevice> availableDevices, boolean recovery) {
-		WechatAdapter.taskType = TaskType.CRAWLER;
-		try {
-			for (AndroidDevice device : availableDevices) {
-				Dao<SubscribeAccount, String> dao = DaoManager.getDao(SubscribeAccount.class);
+    /**
+     * 获取到可用的设备   开启线程池进行任务分配
+     * 任务分配有两种: 关注公众号和抓取特定公众号文章
+     *
+     * @param type
+     */
+    public void allotTask(TaskType type) {
+        List<AndroidDevice> availableDevices = obtainAvailableDevices();
+        if (TaskType.SUBSCRIBE.equals(type)) {
+            allotSubscribeTask(availableDevices);
+        } else if (TaskType.CRAWLER.equals(type)) {
+            allotCrawlerTask(availableDevices, false);
+        }
 
-				//任务队列
-				device.queue.clear();
-				List<SubscribeAccount> subscribeAccounts =
-						dao.queryBuilder().
-								where().eq("udid", device.udid).
-								and().eq("status", SubscribeAccount.CrawlerState.NOFINISH.status).
-								query();
+    }
 
-				for (SubscribeAccount var : subscribeAccounts) {
-					device.queue.add(var.media_name);
-				}
-				//重置点击生效标记
-				device.setClickEffect(false);
-				WechatAdapter adapter = new WechatAdapter(device);
-				adapter.start(recovery);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * Description:  分配订阅公众号的任务
+     *
+     * @param availableDevices
+     */
+    private void allotSubscribeTask(List<AndroidDevice> availableDevices) {
+        try {
+            for (AndroidDevice device : availableDevices) {
 
-	/**
-	 * 当前任务类型
-	 */
-	public enum TaskType {
-		SUBSCRIBE(1),
-		CRAWLER(2);
+                Dao<SubscribeAccount, String> dao = DaoManager.getDao(SubscribeAccount.class);
 
-		private int type;
+                //查询总共关注了多少公众号  今天关注了多少
+                long countSub = dao.queryBuilder().where().
+                        eq("udid", device.udid).countOf();
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(new Date());
+                calendar.set(Calendar.HOUR_OF_DAY, 0);
+                calendar.set(Calendar.MINUTE, 0);
+                calendar.set(Calendar.SECOND, 0);
+                Date var0 = calendar.getTime();
+                Date var1 = DateUtils.addDays(var0, 1);
+                long currentSub = dao.queryBuilder().where().
+                        eq("udid", device.udid).and().
+                        between("insert_time", var0, var1).countOf();
+                if (countSub >= 920 || currentSub >= 40) {
+                    continue;
+                }
 
-		TaskType(int type) {
-			this.type = type;
-		}
-	}
+                //初始化设备的任务队列
+                device.queue.clear();
+                int rest = (int) (40 - currentSub);
+                for (int i = 0; i < rest; i++) { // TODO 测试改动
+                    if (originalAccounts.size() == 0) break;
+                    device.queue.add(originalAccounts.take());
+                }
+                //开启任务执行
+                if (device.queue.size() == 0) return;
 
-	//初始化设备
-	static {
-		String[] var = AndroidUtil.obtainDevices();
-		Random random = new Random();
-		for (int i = 0; i < var.length; i++) {
-			AndroidDevice device = new AndroidDevice(var[i], random.nextInt(50000));
-			device.state = AndroidDevice.State.INIT;
-			device.initApp(DEFAULT_LOCAL_PROXY_PORT + i);
-			devices.put(var[i], device);
-		}
-	}
+                device.setClickEffect(false);
+
+                WechatAdapter adapter = new WechatAdapter(device);
+
+                WechatAdapter.taskType = TaskType.SUBSCRIBE;
+
+                adapter.start(false);
+
+                logger.info("任务初始化完毕！！！");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Description:  抓取文章任务
+     *
+     * @param availableDevices
+     * @param recovery         恢复机制
+     */
+    public void allotCrawlerTask(List<AndroidDevice> availableDevices, boolean recovery) {
+        WechatAdapter.taskType = TaskType.CRAWLER;
+        try {
+            for (AndroidDevice device : availableDevices) {
+                Dao<SubscribeAccount, String> dao = DaoManager.getDao(SubscribeAccount.class);
+
+                //任务队列
+                device.queue.clear();
+                List<SubscribeAccount> subscribeAccounts =
+                        dao.queryBuilder().
+                                where().eq("udid", device.udid).
+                                and().eq("status", SubscribeAccount.CrawlerState.NOFINISH.status).
+                                query();
+
+                for (SubscribeAccount var : subscribeAccounts) {
+                    device.queue.add(var.media_name);
+                }
+                //重置点击生效标记
+                device.setClickEffect(false);
+                WechatAdapter adapter = new WechatAdapter(device);
+                adapter.start(recovery);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 当前任务类型
+     */
+    public enum TaskType {
+        SUBSCRIBE(1),
+        CRAWLER(2);
+
+        private int type;
+
+        TaskType(int type) {
+            this.type = type;
+        }
+    }
+
+    //初始化设备
+    static {
+        try {
+            subscribeDao = DaoManager.getDao(SubscribeAccount.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String[] var = AndroidUtil.obtainDevices();
+        Random random = new Random();
+        for (int i = 0; i < var.length; i++) {
+            AndroidDevice device = new AndroidDevice(var[i], random.nextInt(50000));
+            device.state = AndroidDevice.State.INIT;
+            device.initApp(DEFAULT_LOCAL_PROXY_PORT + i);
+            devices.put(var[i], device);
+        }
+    }
+
+
+    /**
+     * 更细致的任务分配   By Device
+     * Dynamic set device state
+     */
+    public void uncertainAllotTask() {
+        running = true;
+        List<AndroidDevice> availableDevices = obtainAvailableDevices();
+        for (AndroidDevice device : availableDevices) {
+
+        }
+    }
+
+
+    public void uncertainAllotTask(TaskType taskType) {
+
+    }
+
+
+    public void calcuState(String udid) throws Exception {
+
+        //所有订阅公众号的总数量
+        long allSubscribe = subscribeDao.queryBuilder().where().eq("udid", udid).countOf();
+
+        //今日订阅的公众号数量
+    }
 
 }
--- src/test/java/one/rewind/android/automator/test/db/DBTest.java
+++ src/test/java/one/rewind/android/automator/test/db/DBTest.java
@@ -9,72 +9,90 @@ import one.rewind.db.Refacter;
 import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class DBTest {
 
-	@Test
-	public void setupTables() throws Exception {
-		String packageName = "one.rewind.android.automator.model";
-		Refacter.dropTables(packageName);
-		Refacter.createTables(packageName);
-	}
+    @Test
+    public void setupTables() throws Exception {
+        String packageName = "one.rewind.android.automator.model";
+        Refacter.dropTables(packageName);
+        Refacter.createTables(packageName);
+    }
 
-	@Test
-	public void setupTable() throws Exception {
-		Refacter.dropTable(Comments.class);
-		Refacter.createTable(Comments.class);
-		Refacter.dropTable(Essays.class);
-		Refacter.createTable(Essays.class);
-	}
-
-
-	@Test
-	public void setupRawTable() throws Exception {
-		Refacter.createTable(Comments.class);
-	}
+    @Test
+    public void setupTable() throws Exception {
+        Refacter.dropTable(Comments.class);
+        Refacter.createTable(Comments.class);
+        Refacter.dropTable(Essays.class);
+        Refacter.createTable(Essays.class);
+    }
 
 
-	@Test
-	public void testSQLInject() throws Exception {
-		Dao<Essays, String> dao = DaoManager.getDao(Essays.class);
-		long countOf = dao.queryBuilder().where().eq("media_nick", "IPP评论").countOf();
-		System.out.println(countOf);
-	}
-
-	@Test
-	public void queryByCondition() throws Exception {
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(new Date());
-		calendar.set(Calendar.HOUR_OF_DAY, 0);
-		calendar.set(Calendar.MINUTE, 0);
-		calendar.set(Calendar.SECOND, 0);
-		Date zero = calendar.getTime();
-		Date current = DateUtils.addDays(zero, 1);
-		Dao<SubscribeAccount, String> dao = DaoManager.getDao(SubscribeAccount.class);
-		long count = dao.queryBuilder().where().between("insert_time", zero, current).countOf();
-		System.out.println(count);
-	}
+    @Test
+    public void setupRawTable() throws Exception {
+        Refacter.createTable(Comments.class);
+    }
 
 
-	@Test
-	public void updateData() throws Exception {
-		Dao<SubscribeAccount, String> dao = DaoManager.getDao(SubscribeAccount.class);
+    @Test
+    public void testSQLInject() throws Exception {
+        Dao<Essays, String> dao = DaoManager.getDao(Essays.class);
+        long countOf = dao.queryBuilder().where().eq("media_nick", "IPP评论").countOf();
+        System.out.println(countOf);
+    }
 
-		SubscribeAccount subscribeAccount = dao.queryBuilder().where().
-				eq("udid", "ZX1G22PQLH").
-				and().
-				eq("media_name", "北京理工大学研究生教育").
-				queryForFirst();
+    @Test
+    public void queryByCondition() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        Date zero = calendar.getTime();
+        Date current = DateUtils.addDays(zero, 1);
+        Dao<SubscribeAccount, String> dao = DaoManager.getDao(SubscribeAccount.class);
+        long count = dao.queryBuilder().where().between("insert_time", zero, current).countOf();
+        System.out.println(count);
+    }
 
-		System.out.println(subscribeAccount);
 
-		subscribeAccount.status = SubscribeAccount.CrawlerState.FINISH.status;
+    @Test
+    public void updateData() throws Exception {
+        Dao<SubscribeAccount, String> dao = DaoManager.getDao(SubscribeAccount.class);
 
-		System.out.println(subscribeAccount);
+        SubscribeAccount subscribeAccount = dao.queryBuilder().where().
+                eq("udid", "ZX1G22PQLH").
+                and().
+                eq("media_name", "北京理工大学研究生教育").
+                queryForFirst();
 
-		subscribeAccount.update();
-	}
+        System.out.println(subscribeAccount);
+
+        subscribeAccount.status = SubscribeAccount.CrawlerState.FINISH.status;
+
+        System.out.println(subscribeAccount);
+
+        subscribeAccount.update();
+    }
+
+    @Test
+    public void byTimeQuery() throws Exception {
+        Dao<SubscribeAccount, String> dao = DaoManager.getDao(SubscribeAccount.class);
+
+        Date date = new Date();
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+        String result = df.format(date);
+
+        List<SubscribeAccount> insert_time = dao.queryBuilder().where().eq("insert_time", result).query();
+
+        System.out.println(insert_time.size());
+
+    }
 
 }
