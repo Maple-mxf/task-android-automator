c83a5d87fe89c2adde9f37cb865b2c3cd56a7ca1
maxfeng <17793873123@163.com>
2018-11-06 15:43:34 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/one/rewind/android/automator/util/AndroidUtil.java
    #	src/main/java/one/rewind/android/automator/util/DBUtil.java

--- src/test/java/one/rewind/android/automator/test/file/FileTest.java
+++ src/test/java/one/rewind/android/automator/test/file/FileTest.java
@@ -3,6 +3,8 @@ package one.rewind.android.automator.test.file;
 import org.junit.Test;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
@@ -17,5 +19,32 @@ public class FileTest {
         File file = new File(property);
         boolean b = file.canRead();
         System.out.println(b);
+    }
+
+    @Test
+    public void testImageCount() {
+        File file = new File("");
+        Date tmp = new Date();
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy--MM-dd");
+
+        String format = df.format(tmp);
+
+        if (file.isDirectory()) {
+            int count = 0;
+            File[] files = file.listFiles();
+            for (int i = 0; i < files.length; i++) {
+
+                long lastModified = files[i].lastModified();
+
+                Date date = new Date(lastModified);
+
+                if (df.format(date).equals(format)) {
+                    count++;
+                }
+
+            }
+            System.out.println(count);
+        }
     }
 }
