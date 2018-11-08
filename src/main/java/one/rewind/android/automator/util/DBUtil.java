4f5671b4ce1bbf8f1e6411eaf040270b579c9a9d
maxfeng <17793873123@163.com>
2018-11-08 14:04:32 +0800

    BUG改进

--- src/main/java/one/rewind/android/automator/util/DBUtil.java
+++ src/main/java/one/rewind/android/automator/util/DBUtil.java
@@ -1,11 +1,9 @@
 package one.rewind.android.automator.util;
 
-import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.GenericRawResults;
+import one.rewind.android.automator.DBTab;
 import one.rewind.android.automator.adapter.WechatAdapter;
-import one.rewind.android.automator.model.Essays;
 import one.rewind.android.automator.model.SubscribeMedia;
-import one.rewind.db.DaoManager;
 
 import java.sql.*;
 import java.util.List;
@@ -17,40 +15,32 @@ import java.util.stream.Collectors;
  */
 public class DBUtil {
 
-    private static Dao<SubscribeMedia, String> subscribeDao;
-
-    private static Dao<Essays, String> essaysDao;
 
     private static Connection conn;
-
-    static {
-        try {
-            subscribeDao = DaoManager.getDao(SubscribeMedia.class);
-            essaysDao = DaoManager.getDao(Essays.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 
     /**
      * @throws SQLException
      * @see WechatAdapter#subscribeWxAccount(java.lang.String)
      */
     public static void reset() throws SQLException {
-        List<SubscribeMedia> accounts = subscribeDao.queryForAll();
+        List<SubscribeMedia> accounts = DBTab.subscribeDao.queryForAll();
         for (SubscribeMedia v : accounts) {
             try {
                 if (v.status == 2) {
                     continue;
                 }
-                long countOf = essaysDao.queryBuilder().where().eq("media_name", v.media_name).countOf();
-                if ((countOf - v.number) > 5) {
-                    v.status = 0;
-                    v.retry_count = 0;
-                } else {
+                long countOf = DBTab.subscribeDao.
+                        queryBuilder().
+                        where().
+                        eq("media_name", v.media_name).
+                        countOf();
+                if (countOf >= v.number || Math.abs(v.number - countOf) <= 5) {
                     v.retry_count = 5;
-                    v.status = 1;
+                    v.status = SubscribeMedia.CrawlerState.FINISH.status;
+                    v.number = (int) countOf;
+                } else {
+                    v.status = SubscribeMedia.CrawlerState.NOFINISH.status;
+                    v.retry_count = 0;
                 }
                 v.update();
             } catch (Exception e) {
@@ -73,17 +63,19 @@ public class DBUtil {
         return conn;
     }
 
-    /**
-     * 第一个参数是分页参数   每次限定20个公众号
-     */
-    public static void sendAccounts(Set<String> accounts, int page) {
+    private static void sendAccounts(Set<String> accounts, int page) {
         try {
             Connection connection = getConnection();
             PreparedStatement ps =
                     connection.prepareStatement("select name,nick from media limit ?,80");
             ps.setInt(1, page);
             ResultSet rs = ps.executeQuery();
-            Set<String> collect = subscribeDao.queryForAll().stream().map(ec -> ec.media_name).collect(Collectors.toSet());
+            Set<String> collect = DBTab.subscribeDao.
+                    queryForAll().
+                    stream().
+                    map(ec -> ec.media_name).
+                    collect(Collectors.toSet());
+
             while (rs.next()) {
                 String media_nick = rs.getString("nick");
                 if (!collect.contains(media_nick)) {
@@ -117,8 +109,9 @@ public class DBUtil {
      * @return
      */
     public static int obtainSubscribeNumToday(String udid) throws SQLException {
-        GenericRawResults<String[]> results = subscribeDao.queryRaw("select count(id) as number from wechat_subscribe_account where udid = ? and to_days(insert_time) = to_days(NOW())",
-                udid);
+        GenericRawResults<String[]> results = DBTab.subscribeDao.
+                queryRaw("select count(id) as number from wechat_subscribe_account where udid = ? and to_days(insert_time) = to_days(NOW())",
+                        udid);
         String[] firstResult = results.getFirstResult();
         String var = firstResult[0];
         return Integer.parseInt(var);
