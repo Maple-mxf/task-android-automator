617850b17e6d242dff2489d73ff9378b678184e6
maxfeng <m17793873123@163.com>
2018-11-12 14:51:53 +0800

    BUG

--- src/main/java/one/rewind/android/automator/util/ShellUtil.java
+++ src/main/java/one/rewind/android/automator/util/ShellUtil.java
@@ -69,7 +69,7 @@ public class ShellUtil {
 
 
     public static void shutdownProcess(String udid, String packageName) throws IOException, InterruptedException {
-        String command = "adb -s " + udid + " shell am force-stop " + "com.tencent.mm";
+        String command = "adb -s " + udid + " shell am force-stop " + packageName;
         Runtime.getRuntime().exec(command);
         Thread.sleep(5000);
     }
