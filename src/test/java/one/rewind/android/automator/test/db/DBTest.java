4da35fb69d6a43fdb9d643f167844ee431b489ff
maxfeng <m17793873123@163.com>
2018-11-23 14:39:53 +0800

    Merge branch 'master' of http://uml.ink:2333/data-src/wechat-android-automator
    # Conflicts:
    #	src/main/java/one/rewind/android/automator/adapter/LooseWechatAdapter3.java
    #	src/main/java/one/rewind/android/automator/manager/Manager.java

--- src/test/java/one/rewind/android/automator/test/db/DBTest.java
+++ src/test/java/one/rewind/android/automator/test/db/DBTest.java
@@ -10,9 +10,9 @@ import org.junit.Test;
 
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DBTest {
 
@@ -159,6 +159,31 @@ public class DBTest {
         String lastString = tempStr.substring(last);
         lastString = lastString.replaceAll("src=\"", "").replaceAll("\"", "");
         return lastString;
+    }
+
+    /**
+     * 得到网页中图片的地址
+     */
+    public static Set<String> getImgStr(String htmlStr) {
+        Set<String> pics = new HashSet<String>();
+        String img = "";
+        Pattern p_image;
+        Matcher m_image;
+        // String regEx_img = "<img.*src=(.*?)[^>]*?>"; //图片链接地址
+        String regEx_img = "<img.*src\\s*=\\s*(.*?)[^>]*?>";
+        p_image = Pattern.compile
+                (regEx_img, Pattern.CASE_INSENSITIVE);
+        m_image = p_image.matcher(htmlStr);
+        while (m_image.find()) {
+            // 得到<img />数据
+            img = m_image.group();
+// 匹配<img>中的src数据
+            Matcher m = Pattern.compile("src\\s*=\\s*\"?(.*?)(\"|>|\\s+)").matcher(img);
+            while (m.find()) {
+                pics.add(m.group(1));
+            }
+        }
+        return pics;
     }
 
 
